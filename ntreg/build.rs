1f8d8318a4268325df6a6d7ebdbd3d36386d4252
Michael Lombardi <mlombardi@microsoft.com>
2023-02-13 15:25:50 -0600

    Conditional use of PAL on OneCore

--- ntreg/build.rs
+++ ntreg/build.rs
@@ -1,7 +1,14 @@
+#[cfg(onecore)]
 fn main() {
     // Prevent this build script from rerunning unnecessarily.
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-link-lib=onecore_apiset");
     println!("cargo:rustc-link-lib=onecoreuap_apiset");
+    static_vcruntime::metabuild();
+}
+
+#[cfg(not(onecore))]
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
     static_vcruntime::metabuild();
 }
