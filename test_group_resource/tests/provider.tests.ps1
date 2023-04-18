aa0b3faeb44cb5b7c39415f28aab8309d2056462
Steve Lee <slee@microsoft.com>
2023-04-18 06:50:00 -0700

    add `config` property to `provider` in schema

--- test_group_resource/tests/provider.tests.ps1
+++ test_group_resource/tests/provider.tests.ps1
@@ -43,7 +43,8 @@ Describe 'Resource provider tests' {
                     "args": [
                         "listmissingrequires"
                     ]
-                }
+                },
+                "config": "sequence"
             }
         }
 '@
