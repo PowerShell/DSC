8e9e2f6e4927bb9d6eb2bf0f7cb717eddcc309ad
Andrew Menagarishvili <anmenaga@ntdev.microsoft.com>
2023-05-27 12:25:11 -0700

    Added ResourceCache to PSGR

--- powershellgroup/powershellgroup.resource.ps1
+++ powershellgroup/powershellgroup.resource.ps1
@@ -11,6 +11,24 @@ param(
 
 $ProgressPreference = 'Ignore'
 $WarningPreference = 'Ignore'
+$script:ResourceCache = @{}
+
+function RefreshCache
+{
+    $script:ResourceCache = @{}
+
+    $DscResources = Get-DscResource
+    
+    foreach ($r in $DscResources)
+    {
+        $moduleName = "";
+        if ($r.ModuleName) { $moduleName = $r.ModuleName }
+        elseif ($r.ParentPath) { $moduleName = Split-Path $r.ParentPath | Split-Path | Split-Path -Leaf }
+
+        $fullResourceTypeName = "$moduleName/$($r.ResourceType)"
+        $script:ResourceCache[$fullResourceTypeName] = $r
+    }
+}
 
 if ($Operation -eq 'List')
 {
@@ -36,8 +54,11 @@ if ($Operation -eq 'List')
             $propertyList = @($r.Properties.Name)
         }
 
+        $fullResourceTypeName = "$moduleName/$($r.ResourceType)"
+        $script:ResourceCache[$fullResourceTypeName] = $r
+
         $z = [pscustomobject]@{
-            type = "$moduleName/$($r.ResourceType)";
+            type = $fullResourceTypeName;
             version = $version_string;
             path = $r.Path;
             directory = $r.ParentPath;
@@ -55,14 +76,26 @@ elseif ($Operation -eq 'Get')
     $inputobj_pscustomobj = $stdinput | ConvertFrom-Json
 
     $result = @()
+
+    RefreshCache
+
     if ($inputobj_pscustomobj.resources) # we are processing a config batch
     {
         foreach($r in $inputobj_pscustomobj.resources)
         {
-            $inputht = @{}
-            $ResourceTypeName = ($r.type -split "/")[1]
-            $r.properties.psobject.properties | %{ $inputht[$_.Name] = $_.Value }
-            $result += Invoke-DscResource -Method Get -Name $ResourceTypeName -Property $inputht
+            Write-Output $r.type
+            $cachedResourceInfo = $script:ResourceCache[$r.type]
+            if ($cachedResourceInfo)
+            {
+                $inputht = @{}
+                $ResourceTypeName = ($r.type -split "/")[1]
+                $r.properties.psobject.properties | %{ $inputht[$_.Name] = $_.Value }
+                $result += Invoke-DscResource -Method Get -Name $ResourceTypeName -Property $inputht
+            }
+            else
+            {
+                Write-Output "Can not find type in cache"
+            }
         }
     }
 
