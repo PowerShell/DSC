e5c04bc155fc5a2d189f30d5fd58861b40752432
Andrew <anmenaga@microsoft.com>
2023-06-21 14:44:17 -0700

    Updated config tests

--- powershellgroup/Tests/powershellgroup.config.tests.ps1
+++ powershellgroup/Tests/powershellgroup.config.tests.ps1
@@ -21,4 +21,22 @@ Describe 'PowerShellGroup resource tests' {
         $res.results[0].result.actual_state[0].PublishLocation | Should -BeExactly 'https://www.powershellgallery.com/api/v2/package/'
         $res.results[0].result.actual_state[1].Prop1 | Should -BeExactly 'ValueForProp1'
     }
+
+    It 'Test works on config with class-based and script-based resources' {
+        
+        $r = Get-Content -Raw $configPath | dsc config test
+        $LASTEXITCODE | Should -Be 0
+        $res = $r | ConvertFrom-Json
+        $res.results[0].result.actual_state[0] | Should -Not -BeNull
+        $res.results[0].result.actual_state[1] | Should -Not -BeNull
+    }
+
+    It 'Set works on config with class-based and script-based resources' {
+        
+        $r = Get-Content -Raw $configPath | dsc config set
+        $LASTEXITCODE | Should -Be 0
+        $res = $r | ConvertFrom-Json
+        $res.results.result.after_state[0].RebootRequired | Should -Not -BeNull
+        $res.results.result.after_state[1].RebootRequired | Should -Not -BeNull
+    }
 }
