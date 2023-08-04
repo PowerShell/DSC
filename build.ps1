dacaeafc56cbe5ff44ac95baed0a1e17a6b0473e
Steve Lee <slee@microsoft.com>
2023-08-04 13:45:23 -0700

    enable building for more archs

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@
 
 param(
     [switch]$Release,
-    [ValidateSet('none','aarch64-pc-windows-msvc','x86_64-pc-windows-msvc')]
+    [ValidateSet('none','aarch64-pc-windows-msvc','x86_64-pc-windows-msvc','aarch64-apple-darwin','aarch64-unknown-linux-gnu','aarch64-unknown-linux-musl','x86_64-unknown-linux-gnu','x86_64-unknown-linux-musl')]
     $architecture = 'none',
     [switch]$Clippy,
     [switch]$Test
