71853ea752a5b5d0acf9b256c6d63a06c4d2c095
Andrew <anmenaga@microsoft.com>
2023-08-23 17:55:36 -0700

    ProcessList resource 3

--- process/src/main.rs
+++ process/src/main.rs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 mod process_info;
+use std::env;
+use std::process::exit;
 
 #[cfg(windows)]
 use tasklist;
@@ -31,33 +33,53 @@ fn print_task_list() {
     for prc in procfs::process::all_processes().unwrap() {
         
         let mut p = process_info::ProcessInfo::new();
-        let proc = prc.as_ref().expect("proc not found");
+        let proc = prc.as_ref().expect("Can't get all_processes");
         p.pid = proc.pid() as u32;
         p.name = match proc.exe() {
-            Ok(exe) => { String::from(exe.file_name().expect("Can't get filename").to_str().unwrap()) }
+            Ok(exe) => { String::from(exe.file_name().expect("Can't get process filename").to_str().unwrap()) }
             Err(_) => { String::from("") }
         };
 
+        if p.name == "" { continue; };
 
         p.cmdline = match proc.cmdline() {
-            Ok(cmdline_vector) => {
-                //for arg in cmdline_vector
-                //{ println!("{arg}"); 
-                //}
-
-                cmdline_vector.join(" ")
-            }
+            Ok(cmdline_vector) => { cmdline_vector.join(" ") }
             Err(_) => { String::from("") }
         };
 
-	if p.name != ""
-        {
-           let json = serde_json::to_string(&p).unwrap();
-           println!("{json}");
-        };
+        let json = serde_json::to_string(&p).unwrap();
+        println!("{json}");
     }
 }
 
+fn help() {
+    println!("usage: process list");
+}
+
 fn main() {
-    print_task_list();
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        // one argument passed
+        2 => {
+            match args[1].as_str() {
+                "list" => {
+                    print_task_list();
+                    exit(0);
+                },
+                "set" => { // used for testing only
+                    println!("{{\"result\":\"Ok\"}}");
+                    exit(0);
+                },
+                _ => {
+                    help();
+                    exit(1);
+                },
+            }
+        },
+        // all the other cases
+        _ => {
+            help();
+            exit(1);
+        }
+    }
 }
