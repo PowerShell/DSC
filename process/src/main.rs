72c8e6a95ea780ac68715dd045a55b4cc03f9d47
anmenaga <anmenaga@microsoft.com>
2023-08-23 17:35:15 -0700

    Updated Linux implementation

--- process/src/main.rs
+++ process/src/main.rs
@@ -31,13 +31,30 @@ fn print_task_list() {
     for prc in procfs::process::all_processes().unwrap() {
         
         let mut p = process_info::ProcessInfo::new();
-        p.pid = prc.pid();
-        //p.name = i.get_pname();
-        //p.cmdline = i.get_cmd_params();
+        let proc = prc.as_ref().expect("proc not found");
+        p.pid = proc.pid() as u32;
+        p.name = match proc.exe() {
+            Ok(exe) => { String::from(exe.file_name().expect("Can't get filename").to_str().unwrap()) }
+            Err(_) => { String::from("") }
+        };
 
-        let json = serde_json::to_string(&p).unwrap();
-        println!("{json}");
-        }
+
+        p.cmdline = match proc.cmdline() {
+            Ok(cmdline_vector) => {
+                //for arg in cmdline_vector
+                //{ println!("{arg}"); 
+                //}
+
+                cmdline_vector.join(" ")
+            }
+            Err(_) => { String::from("") }
+        };
+
+	if p.name != ""
+        {
+           let json = serde_json::to_string(&p).unwrap();
+           println!("{json}");
+        };
     }
 }
 
