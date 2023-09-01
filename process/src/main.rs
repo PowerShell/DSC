0265beb92b569f0e067eb22bb70033bf7b0ecf76
Andrew <anmenaga@microsoft.com>
2023-09-01 14:06:28 -0700

    Updated Process 1

--- process/src/main.rs
+++ process/src/main.rs
@@ -4,21 +4,24 @@
 mod process_info;
 use std::env;
 use std::process::exit;
+use std::io::{self, Read};
 use sysinfo::{ProcessExt, System, SystemExt, PidExt};
+use crate::process_info::ProcessInfo;
 
-fn print_task_list() {
-
+fn get_task_list() -> Vec<ProcessInfo>
+{
+    let mut result = Vec::new();
     let mut s = System::new();
     s.refresh_processes();
     for (pid, process) in s.processes() {
-        let mut p = process_info::ProcessInfo::new();
+        let mut p = ProcessInfo::new();
         p.pid = pid.as_u32();
         p.name = String::from(process.name());
         p.cmdline = format!("{:?}", process.cmd());
-
-        let json = serde_json::to_string(&p).unwrap();
-        println!("{json}");
+        result.push(p);
     }
+
+    result
 }
 
 fn help() {
@@ -31,11 +34,25 @@ fn main() {
         // one argument passed
         match args[1].as_str() {
             "list" => {
-                print_task_list();
+                for p in get_task_list()
+                {
+                    let json = serde_json::to_string(&p).unwrap();
+                    println!("{json}");
+                }
+                exit(0);
+            },
+            "get" => { // used for testing only
+                let mut buffer: Vec<u8> = Vec::new();
+                io::stdin().read_to_end(&mut buffer).unwrap();
+                let input = String::from_utf8(buffer);
+                println!("{}", input.unwrap());
                 exit(0);
             },
             "set" => { // used for testing only
-                println!("{{\"result\":\"Ok\"}}");
+                let mut buffer: Vec<u8> = Vec::new();
+                io::stdin().read_to_end(&mut buffer).unwrap();
+                let input = String::from_utf8(buffer);
+                println!("{}", input.unwrap());
                 exit(0);
             },
             _ => {
