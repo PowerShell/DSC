6c1c6d6670866aefb44cbce130234e8aaa4d3e12
Steve Lee <slee@microsoft.com>
2023-09-26 16:01:16 -0700

    Fix `add-path.ps1` to use $PSScriptRoot instead of $PWD

--- tools/add-path.ps1
+++ tools/add-path.ps1
@@ -1,12 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-# This script will add the current directory to the PATH environment variable
+# This script will add the script directory to the PATH environment variable
 # for the current user. This is useful for development purposes.
 
 $pathSeparator = [System.IO.Path]::PathSeparator
 $paths = $env:PATH.Split($pathSeparator)
-if ($paths -notcontains $PWD) {
-    $env:PATH = "$PWD" + $pathSeparator + $env:PATH
-    Write-Host -ForegroundColor Green "Added $PWD to `$env:PATH"
+if ($paths -notcontains $PSScriptRoot) {
+    $env:PATH = "$PSScriptRoot" + $pathSeparator + $env:PATH
+    Write-Host -ForegroundColor Green "Added $PSScriptRoot to `$env:PATH"
 }
