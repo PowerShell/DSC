85cc089aaa664198e00854247a6c7f257210fbe6
Steve Lee <slee@microsoft.com>
2023-10-09 18:12:15 -0700

    update build script to install dependencies

--- tree-sitter-dscexpression/build.ps1
+++ tree-sitter-dscexpression/build.ps1
@@ -10,6 +10,20 @@ function Invoke-NativeCommand($cmd) {
     }
 }
 
+if ($null -eq (Get-Command npm -ErrorAction Ignore)) {
+    throw "npm is not installed"
+}
+
+npm list tree-sitter-cli
+if ($LASTEXITCODE -ne 0) {
+    npm install tree-sitter-cli
+}
+
+npm list node-gyp
+if ($LASTEXITCODE -ne 0) {
+    npm install node-gyp
+}
+
 Invoke-NativeCommand 'npx tree-sitter generate'
 Invoke-NativeCommand 'node-gyp configure'
 Invoke-NativeCommand 'node-gyp build'
