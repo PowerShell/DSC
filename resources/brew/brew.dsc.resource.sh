00485d08194f1749cd1ede826bca06a38c4f41eb
Steve Lee <slee@microsoft.com>
2023-10-23 20:55:54 -0700

    address Jim's feedback

--- resources/brew/brew.dsc.resource.sh
+++ resources/brew/brew.dsc.resource.sh
@@ -6,6 +6,8 @@
 export exist=true
 export NONINTERACTIVE=1
 
+# $packageName and $_exist are sent as env vars by dsc converting the JSON input to name/value pairs
+
 check_args() {
     if [[ -z $packageName ]]; then
         echo "packageName not set"
@@ -20,19 +22,20 @@ to_json() {
 }
 
 if [ $# -eq 0 ]; then
-    echo "Command not provided, valid commands: get, export"
+    echo "Command not provided, valid commands: get, set, export"
     exit 1
 elif [[ $1 == "get" ]]; then
     check_args
     output="$(brew list ${packageName} --versions)"
     if [[ $? -ne 0 ]]; then
-        export exist=false
+        exist=false
         output="${packageName}"
     fi
     echo $output | to_json
 elif [[ $1 == "set" ]]; then
     check_args
     if [[ -z $_exist ]]; then
+        # if $_exist is not defined in the input, it defaults to `true`
         _exist=true
     fi
     if [[ $_exist = true ]]; then
@@ -43,6 +46,6 @@ elif [[ $1 == "set" ]]; then
 elif [[ $1 == "export" ]]; then
     brew list --versions | to_json
 else
-    echo "Invalid command, valid commands: get, export"
+    echo "Invalid command, valid commands: get, set, export"
     exit 1
 fi
