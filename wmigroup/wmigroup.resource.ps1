2f8b303213311fda589d0d9d119c4a77311d5778
Andrew <anmenaga@microsoft.com>
2023-11-15 22:17:54 -0800

    Added WMI provider

--- wmigroup/wmigroup.resource.ps1
+++ wmigroup/wmigroup.resource.ps1
@@ -51,6 +51,78 @@ if ($Operation -eq 'List')
         $z | ConvertTo-Json -Compress
     }
 }
+elseif ($Operation -eq 'Get')
+{
+    $inputobj_pscustomobj = $null
+    if ($stdinput)
+    {
+        $inputobj_pscustomobj = $stdinput | ConvertFrom-Json
+    }
+
+    $result = @()
+
+    if ($inputobj_pscustomobj.resources) # we are processing a config batch
+    {
+        foreach($r in $inputobj_pscustomobj.resources)
+        {
+            $type_fields = $r.type -split "/"
+            $wmi_namespace = $type_fields[0]
+            $wmi_classname = $type_fields[1]
+
+            #TODO: add filtering based on supplied properties of $r
+            $wmi_instances = Get-CimInstance -Namespace $wmi_namespace -ClassName $wmi_classname
+
+            if ($wmi_instances)
+            {
+                $instance_result = @{}
+                $wmi_instance = $wmi_instances[0] # for 'Get' we return just first matching instance; for 'export' we return all instances
+                $wmi_instance.psobject.properties | %{ 
+                    if (($_.Name -ne "type") -and (-not $_.Name.StartsWith("Cim")))
+                    {
+                        $instance_result[$_.Name] = $_.Value
+                    }
+                }
+
+                $result += @($instance_result)
+            }
+            else
+            {
+                $errmsg = "Can not find type " + $r.type + "; please ensure that Get-CimInstance returns this resource type"
+                Write-Error $errmsg
+                exit 1
+            }
+        }
+    }
+    else # we are processing an individual resource call
+    {
+        $type_fields = $inputobj_pscustomobj.type -split "/"
+        $wmi_namespace = $type_fields[0]
+        $wmi_classname = $type_fields[1]
+
+        #TODO: add filtering based on supplied properties of $inputobj_pscustomobj
+        $wmi_instances = Get-CimInstance -Namespace $wmi_namespace -ClassName $wmi_classname
+
+        if ($wmi_instances)
+        {
+            $wmi_instance = $wmi_instances[0] # for 'Get' we return just first matching instance; for 'export' we return all instances
+            $result = @{}
+            $wmi_instance.psobject.properties | %{ 
+                if (($_.Name -ne "type") -and (-not $_.Name.StartsWith("Cim")))
+                {
+                    $result[$_.Name] = $_.Value
+                }
+            }
+        }
+        else
+        {
+            $errmsg = "Can not find type " + $inputobj_pscustomobj.type + "; please ensure that Get-CimInstance returns this resource type"
+            Write-Error $errmsg
+            exit 1
+        }
+    }
+
+    $result | ConvertTo-Json -Compress
+}
 else
 {
     Write-Error "ERROR: Unsupported operation requested from wmigroup.resource.ps1"
