7751a88369f4fa94bec7388cf6d690a877b0f610
Andrew <anmenaga@microsoft.com>
2024-03-07 12:02:04 -0800

    Fixed error in merge conflict

--- dsc/tests/dsc_args.tests.ps1
+++ dsc/tests/dsc_args.tests.ps1
@@ -185,6 +185,7 @@ resources:
         dsc -l trace resource list 2> $TestDrive/tracing.txt
         "$TestDrive/tracing.txt" | Should -FileContentMatchExactly 'PSModulePath'
         $LASTEXITCODE | Should -Be 0
+    }
 
     It 'stdin cannot be empty if neither input or path is provided' {
         '' | dsc resource set -r Microsoft/OSInfo 2> $TestDrive/error.txt
