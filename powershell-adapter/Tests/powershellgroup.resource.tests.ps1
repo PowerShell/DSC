aac9bb2d7a4ecae85d99f12d6a9532a88f4df6e9
Andrew <anmenaga@microsoft.com>
2024-04-02 15:59:15 -0700

    Updated powershellgroup.resource.tests.ps1

--- powershell-adapter/Tests/powershellgroup.resource.tests.ps1
+++ powershell-adapter/Tests/powershellgroup.resource.tests.ps1
@@ -13,7 +13,7 @@ Describe 'PowerShell adapter resource tests' {
 
     It 'Discovery includes class-based and script-based resources ' -Skip:(!$IsWindows){
 
-        $r = dsc resource list * *PowerShell*
+        $r = dsc resource list * -a *PowerShell*
         $LASTEXITCODE | Should -Be 0
         $resources = $r | ConvertFrom-Json
         ($resources | ? {$_.Type -eq 'PSTestModule/TestClassResource'}).Count | Should -Be 1
