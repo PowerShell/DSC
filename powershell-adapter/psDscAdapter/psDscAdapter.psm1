39ff59ccd1b6a5452d0fa7b3da0f3c3281ea66b2
mgreenegit <michael.greene@microsoft.com>
2024-04-08 13:01:50 -0500

    bad param

--- powershell-adapter/psDscAdapter/psDscAdapter.psm1
+++ powershell-adapter/psDscAdapter/psDscAdapter.psm1
@@ -6,7 +6,7 @@
 # PSDesiredStateConfiguration 2.0.7 module will be saved in the DSC build
 # in Windows PowerShell, we should always use version 1.1 that ships in Windows
 if ($PSVersionTable.PSVersion.Major -gt 5) {
-    $PSDesiredStateConfiguration = Import-Module -Path '../PSDesiredStateConfiguration/2.0.7/PSDesiredStateConfiguration.psd1' -Force -PassThru
+    $PSDesiredStateConfiguration = Import-Module "$PSScriptRoot/../PSDesiredStateConfiguration/2.0.7/PSDesiredStateConfiguration.psd1" -Force -PassThru
 }
 else {
     $env:PSModulePath += ";$env:windir\System32\WindowsPowerShell\v1.0\Modules"
