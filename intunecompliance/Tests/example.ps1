903a4cc06654f80fa605fe4c102af632c3880153
mgreenegit <michael.greene@microsoft.com>
2024-04-15 11:13:00 -0500

    implicit remoting

--- intunecompliance/Tests/example.ps1
+++ intunecompliance/Tests/example.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+import-module microsoft.powershell.management -Version 3.1.0.0
 
 $WMI_ComputerSystem = Get-WMIObject -class Win32_ComputerSystem
 $WMI_BIOS = Get-WMIObject -class Win32_BIOS 
