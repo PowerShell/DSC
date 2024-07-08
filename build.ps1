0f79ebfbf66e73d58b12670b2f47d7ff7dc7e975
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2024-07-08 15:38:51 -0700

    move cratesio definition to env var

--- build.ps1
+++ build.ps1
@@ -175,10 +175,12 @@ if (!$SkipBuild) {
     if (!$UseCFS) {
         # this will override the config.toml
         Write-Host "Setting CARGO_SOURCE_crates-io_REPLACE_WITH to 'crates-io'"
-        ${env:CARGO_SOURCE_crates-io_REPLACE_WITH} = 'cratesio'
+        ${env:CARGO_SOURCE_crates-io_REPLACE_WITH} = 'CRATESIO'
+        $env:CARGO_REGISTRIES_CRATESIO_INDEX = 'sparse+https://index.crates.io/'
     } else {
         Write-Host "Using CFS for cargo source replacement"
         ${env:CARGO_SOURCE_crates-io_REPLACE_WITH} = $null
+        $env:CARGO_REGISTRIES_CRATESIO_INDEX = $null
     }
 
     # make sure dependencies are built first so clippy runs correctly
