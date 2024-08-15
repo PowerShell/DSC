aad9f3b4bb1b42abee7623538417fa2be940ff96
G.Reijn <G.Reijn@enzazaden.nl>
2024-08-15 16:45:34 +0200

    Start working on WMI adapter improvements

--- wmi-adapter/Tests/wmi.tests.ps1
+++ wmi-adapter/Tests/wmi.tests.ps1
@@ -6,7 +6,7 @@ Describe 'WMI adapter resource tests' {
     BeforeAll {
         if ($IsWindows)
         {
-            $OldPSModulePath  = $env:PSModulePath
+            $OldPSModulePath = $env:PSModulePath
             $env:PSModulePath += ";" + $PSScriptRoot
 
             $configPath = Join-path $PSScriptRoot "test_wmi_config.dsc.yaml"
@@ -19,39 +19,58 @@ Describe 'WMI adapter resource tests' {
         }
     }
 
-    It 'List shows WMI resources' -Skip:(!$IsWindows){
+    Context 'List WMI resources' {
+        It 'List shows WMI resources' -Skip:(!$IsWindows) {
 
-        $r = dsc resource list *OperatingSystem* -a Microsoft.Windows/WMI
-        $LASTEXITCODE | Should -Be 0
-        $res = $r | ConvertFrom-Json
-        $res.Count | Should -BeGreaterOrEqual 1
+            $r = dsc resource list *OperatingSystem* -a Microsoft.Windows/WMI
+            $LASTEXITCODE | Should -Be 0
+            $res = $r | ConvertFrom-Json
+            $res.Count | Should -BeGreaterOrEqual 1
+        }   
     }
 
-    It 'Get works on an individual WMI resource' -Skip:(!$IsWindows){
+    Context 'Get WMI resources' {
+        It 'Get works on an individual WMI resource' -Skip:(!$IsWindows) {
 
-        $r = dsc resource get -r root.cimv2/Win32_OperatingSystem
-        $LASTEXITCODE | Should -Be 0
-        $res = $r | ConvertFrom-Json
-        $res.actualState.CreationClassName | Should -Be "Win32_OperatingSystem"
+            $r = dsc resource get -r root.cimv2/Win32_OperatingSystem
+            $LASTEXITCODE | Should -Be 0
+            $res = $r | ConvertFrom-Json
+            $res.actualState.result.type | Should -BeLike "*Win32_OperatingSystem"
+        }
+    
+        It 'Get works on a config with WMI resources' -Skip:(!$IsWindows) {
+    
+            $r = Get-Content -Raw $configPath | dsc config get
+            $LASTEXITCODE | Should -Be 0
+            $res = $r | ConvertFrom-Json
+            $res.results.result.actualstate.result[0].properties.value.LastBootUpTime | Should -Not -BeNull
+            $res.results.result.actualstate.result[0].properties.value.Caption | Should -Not -BeNull
+            $res.results.result.actualstate.result[0].properties.value.NumberOfProcesses | Should -Not -BeNull
+        }
+    
+        It 'Example config works' -Skip:(!$IsWindows) {
+            $configPath = Join-Path $PSScriptRoot '..\..\dsc\examples\wmi.dsc.yaml'
+            $r = dsc config get -p $configPath
+            $LASTEXITCODE | Should -Be 0
+            $r | Should -Not -BeNullOrEmpty
+            $res = $r | ConvertFrom-Json
+            $res.results.result.actualstate.result[0].properties.value.Model | Should -Not -BeNullOrEmpty
+            $res.results.result.actualstate.result[0].properties.value.Description | Should -Not -BeNullOrEmpty
+        }
     }
 
-    It 'Get works on a config with WMI resources' -Skip:(!$IsWindows){
+    # TODO: work on set test
+    # Context "Set WMI resources" {
+    # }
 
-        $r = Get-Content -Raw $configPath | dsc config get
-        $LASTEXITCODE | Should -Be 0
-        $res = $r | ConvertFrom-Json
-        $res.results[0].result.actualState[0].LastBootUpTime | Should -Not -BeNull
-        $res.results[0].result.actualState[1].BiosCharacteristics | Should -Not -BeNull
-        $res.results[0].result.actualState[2].NumberOfLogicalProcessors | Should -Not -BeNull
-    }
-
-    It 'Example config works' -Skip:(!$IsWindows) {
-        $configPath = Join-Path $PSScriptRoot '..\..\dsc\examples\wmi.dsc.yaml'
-        $r = dsc config get -p $configPath
-        $LASTEXITCODE | Should -Be 0
-        $r | Should -Not -BeNullOrEmpty
-        $res = $r | ConvertFrom-Json
-        $res.results[0].result.actualState[0].Model | Should -Not -BeNullOrEmpty
-        $res.results[0].result.actualState[1].Description | Should -Not -BeNullOrEmpty
-    }
+    # TODO: get export working
+    # Context "Export WMI resources" {
+    #     It 'Exports all resources' -Skip:(!$IsWindows) {
+    #         $r = dsc resource export -r root.cimv2/Win32_Process
+    #         $LASTEXITCODE | Should -Be 0
+    #         $res = $r | ConvertFrom-Json
+    #         $res.resources[0].properties.result.count | Should -BeGreaterThan 1
+    #         $json.resources[0].properties.result.properties.value | Should -not -BeNullOrEmpty
+    #     }
+    # }
 }
---
+++ wmi-adapter/wmiAdapter.psd1
@@ -0,0 +1,47 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+@{
+
+    # Script module or binary module file associated with this manifest.
+    RootModule        = 'wmiAdapter.psm1'
+    
+    # Version number of this module.
+    moduleVersion     = '0.0.1'
+    
+    # ID used to uniquely identify this module
+    GUID              = '420c66dc-d243-4bf8-8de0-66467328f4b7'
+    
+    # Author of this module
+    Author            = 'Microsoft Corporation'
+    
+    # Company or vendor of this module
+    CompanyName       = 'Microsoft Corporation'
+    
+    # Copyright statement for this module
+    Copyright         = '(c) Microsoft Corporation. All rights reserved.'
+    
+    # Description of the functionality provided by this module
+    Description       = 'PowerShell Desired State Configuration Module for DSC WMI Adapter'
+    
+    # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
+    FunctionsToExport = @(
+        'Invoke-DscWmi'
+    )
+    
+    # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
+    CmdletsToExport   = @()
+    
+    # Variables to export from this module
+    VariablesToExport = @()
+    
+    # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
+    AliasesToExport   = @()
+    
+    PrivateData       = @{
+        PSData = @{
+            ProjectUri = 'https://github.com/PowerShell/dsc'
+        }
+    }
+}
+    
