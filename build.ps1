8eb3183271556f44bf2ee23d0e1bb98818db74e5
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2024-08-27 16:37:13 -0700

    add tracing of the repo

--- build.ps1
+++ build.ps1
@@ -368,6 +368,7 @@ if ($Test) {
         $FullyQualifiedName = @{ModuleName="PSDesiredStateConfiguration";ModuleVersion="2.0.7"}
         if (-not(Get-Module -ListAvailable -FullyQualifiedName $FullyQualifiedName))
         {
+            write-verbose -verbose "Using '$repository' repository"
             Install-PSResource -Name PSDesiredStateConfiguration -Version 2.0.7 -Repository $repository -Verbose
         }
     }
