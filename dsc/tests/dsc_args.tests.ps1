ae92419bfbfcf25b3734ffa05afe9396b2ac39d4
Andrew <anmenaga@microsoft.com>
2024-08-30 10:42:36 -0700

    feedback 1

--- dsc/tests/dsc_args.tests.ps1
+++ dsc/tests/dsc_args.tests.ps1
@@ -293,7 +293,7 @@ resources:
 
     It 'Test operation on the adapter itself should fail' {
         dsc resource test -r Microsoft.DSC/PowerShell 2> $TestDrive/tracing.txt
-        $LASTEXITCODE | Should -Be 1
+        $LASTEXITCODE | Should -Be 2
         "$TestDrive/tracing.txt" | Should -FileContentMatchExactly 'Can not perform this operation on the adapter'
     }
 
