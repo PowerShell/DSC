f41e6a2d5dfaea995738c2b8057f4ddee2fc45ac
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2024-09-19 12:57:33 -0700

    Add tracing to `registry` resource

--- registry/src/main.rs
+++ registry/src/main.rs
@@ -11,6 +11,8 @@ use clap::Parser;
 use registry_helper::RegistryHelper;
 use schemars::schema_for;
 use std::process::exit;
+use tracing::{debug, error};
+use tracing_subscriber::{EnvFilter, Layer, prelude::__tracing_subscriber_SubscriberExt};
 
 use crate::config::Registry;
 
@@ -28,19 +30,21 @@ fn main() {
     #[cfg(debug_assertions)]
     check_debug();
 
+    enable_tracing();
+
     let args = Arguments::parse();
     match args.subcommand {
         args::SubCommand::Query { key_path, value_name, recurse } => {
-            eprintln!("Get key_path: {key_path}, value_name: {value_name:?}, recurse: {recurse}");
+            debug!("Get key_path: {key_path}, value_name: {value_name:?}, recurse: {recurse}");
         },
         args::SubCommand::Set { key_path, value } => {
-            eprintln!("Set key_path: {key_path}, value: {value}");
+            debug!("Set key_path: {key_path}, value: {value}");
         },
         args::SubCommand::Remove { key_path, value_name, recurse } => {
-            eprintln!("Remove key_path: {key_path}, value_name: {value_name:?}, recurse: {recurse}");
+            debug!("Remove key_path: {key_path}, value_name: {value_name:?}, recurse: {recurse}");
         },
         args::SubCommand::Find { key_path, find, recurse, keys_only, values_only } => {
-            eprintln!("Find key_path: {key_path}, find: {find}, recurse: {recurse:?}, keys_only: {keys_only:?}, values_only: {values_only:?}");
+            debug!("Find key_path: {key_path}, find: {find}, recurse: {recurse:?}, keys_only: {keys_only:?}, values_only: {values_only:?}");
         },
         args::SubCommand::Config { subcommand } => {
             match subcommand {
@@ -48,7 +52,7 @@ fn main() {
                     let reg_helper = match RegistryHelper::new(&input) {
                         Ok(reg_helper) => reg_helper,
                         Err(err) => {
-                            eprintln!("Error: {err}");
+                            error!("{err}");
                             exit(EXIT_INVALID_INPUT);
                         }
                     };
@@ -58,7 +62,7 @@ fn main() {
                             println!("{json}");
                         },
                         Err(err) => {
-                            eprintln!("Error: {err}");
+                            error!("{err}");
                             exit(EXIT_REGISTRY_ERROR);
                         }
                     }
@@ -67,7 +71,7 @@ fn main() {
                     let mut reg_helper = match RegistryHelper::new(&input) {
                         Ok(reg_helper) => reg_helper,
                         Err(err) => {
-                            eprintln!("Error: {err}");
+                            error!("{err}");
                             exit(EXIT_INVALID_INPUT);
                         }
                     };
@@ -82,7 +86,7 @@ fn main() {
                             }
                         },
                         Err(err) => {
-                            eprintln!("Error: {err}");
+                            error!("{err}");
                             exit(EXIT_REGISTRY_ERROR);
                         }
                     }
@@ -91,14 +95,14 @@ fn main() {
                     let reg_helper = match RegistryHelper::new(&input) {
                         Ok(reg_helper) => reg_helper,
                         Err(err) => {
-                            eprintln!("Error: {err}");
+                            error!("{err}");
                             exit(EXIT_INVALID_INPUT);
                         }
                     };
                     match reg_helper.remove() {
                         Ok(()) => {},
                         Err(err) => {
-                            eprintln!("Error: {err}");
+                            error!("{err}");
                             exit(EXIT_REGISTRY_ERROR);
                         }
                     }
@@ -113,6 +117,25 @@ fn main() {
     }
 
     exit(EXIT_SUCCESS);
+}
+
+pub fn enable_tracing() {
+    let filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("trace"))
+        .unwrap_or_default();
+    let layer = tracing_subscriber::fmt::Layer::default().with_writer(std::io::stderr);
+    let fmt = layer
+                .with_ansi(false)
+                .with_level(true)
+                .with_line_number(true)
+                .json()
+                .boxed();
+
+    let subscriber = tracing_subscriber::Registry::default().with(fmt).with(filter);
+
+    if tracing::subscriber::set_global_default(subscriber).is_err() {
+        eprintln!("Unable to set global default tracing subscriber.  Tracing is diabled.");
+    }
 }
 
 #[cfg(debug_assertions)]
