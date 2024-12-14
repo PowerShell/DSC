c2f9135b8244622f0804169622781e709d1661e2
Aditya Patwardhan <adityap@microsoft.com>
2024-12-14 11:05:28 -0800

    Refactor

--- resources/fs/src/file_helper.rs
+++ resources/fs/src/file_helper.rs
@@ -6,38 +6,18 @@ use crate::config::File;
 use std::fs;
 use std::fs::File as fsFile;
 use std::path::Path;
-use tracing::{debug, error};
+use tracing::{debug};
 //use fs_extra::dir::get_size;
 
 pub fn get_file(file: &File) -> Result<File, Box<dyn std::error::Error>> {
-    let resolved_path = Path::new(file.path.as_str());
-    debug!("Resolved path: {:?}", resolved_path);
-    match resolved_path.is_dir() {
-        true => {
-            return Ok(File { path: file.path.to_string(), size: None, hash: String::new(), exist: Some(false) });
-        }
-        false => {}
-    }
-    let f: fsFile = match fsFile::open(resolved_path) {
-        Ok(f) => f,
+    match compare_file_state(file) {
+        Ok(f) => {
+            Ok(f)
+        },
         Err(e) => {
-            if e.kind() == std::io::ErrorKind::NotFound {
-                return Ok(File { path: file.path.to_string(), size: None, hash: String::new(), exist: Some(false) });
-            } else {
-                return Err(e)?
-            }
+            Err(e)?
         }
-    };
-
-    let metadata = f.metadata()?;
-    let hash = calculate_hash(file.path.as_str())?;
-
-    let mut updated_file = file.clone();
-    updated_file.path = file.path.clone();
-    updated_file.size = Some(metadata.len());
-    updated_file.hash = hash;
-    updated_file.exist = Some(true);
-    Ok(updated_file)
+    }
 }
 
 // pub fn export_path(path: &str) -> Result<Directory, Box<dyn std::error::Error>> {
@@ -84,14 +64,66 @@ pub fn get_file(file: &File) -> Result<File, Box<dyn std::error::Error>> {
 // }
 
 pub fn delete_file(file: &File) -> Result<(), Box<dyn std::error::Error>> {
-    let path = file.path.as_str();
-    fs::remove_file(path)?;
-    Ok(())
+    match compare_file_state(file) {
+        Ok(f) => {
+            debug!("Deleting file: {:?}", f.path);
+            fs::remove_file(f.path)?;
+            Ok(())
+        },
+        Err(e) => {
+            Err(e)?
+        }
+    }
+}
+
+fn compare_file_state(file: &File) -> Result<File, Box<dyn std::error::Error>> {
+    let resolved_path = Path::new(file.path.as_str());
+    debug!("Resolved path: {:?}", resolved_path);
+    match resolved_path.is_dir() {
+        true => {
+            debug!("Path is a directory");
+            let mut updated_file = file.clone();
+            updated_file.exist = Some(false);
+            return Ok(updated_file)
+        }
+        false => {}
+    }
+    let f: fsFile = match fsFile::open(resolved_path) {
+        Ok(f) => {
+            debug!("File found: {:?}", file.path);
+            f
+        },
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::NotFound {
+                debug!("File not found: {:?}", file.path);
+                let mut updated_file = file.clone();
+                updated_file.exist = Some(false);
+                return Ok(updated_file)
+            } else {
+                return Err(e)?
+            }
+        }
+    };
+
+    let hash = calculate_hash(file.path.as_str())?;
+
+    if hash.to_lowercase() != file.hash.to_lowercase() {
+        debug!("Hash mismatch: {:?}", file.path);
+        let mut updated_file = file.clone();
+        updated_file.exist = Some(false);
+        return Ok(updated_file)
+    }
+
+    let metadata = f.metadata()?;
+    let mut updated_file = file.clone();
+    updated_file.size = Some(metadata.len());
+    updated_file.exist = Some(true);
+
+    Ok(updated_file)
 }
 
 fn calculate_hash(path: &str) -> Result<String, Box<dyn std::error::Error>> {
     let bytes = fs::read(path)?;
     let digest = sha256::digest(&bytes);
-
     Ok(digest)
 }
