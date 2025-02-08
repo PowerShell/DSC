597fe6fcb9c0b7913a6a9c9a04ed0adbbc086cbd
G.Reijn <G.Reijn@enzazaden.nl>
2025-02-08 05:38:48 +0100

    Fix up formatting

--- dsc/tests/dsc_variables.tests.ps1
+++ dsc/tests/dsc_variables.tests.ps1
@@ -1,46 +1,46 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-Describe 'Configuration variables tests' {
+Describe 'Configruation variables tests' {
   It 'Variables example config works' {
-      $configFile = "$PSScriptRoot/../examples/variables.dsc.yaml"
-      $out = dsc config get -f $configFile | ConvertFrom-Json
-      $LASTEXITCODE | Should -Be 0
-      $out.results[0].result.actualState.output | Should -BeExactly 'myOutput is: Hello world!, myObject is: baz'
+    $configFile = "$PSSCriptRoot/../examples/variables.dsc.yaml"
+    $out = dsc config get -f $configFile | ConvertFrom-Json
+    $LASTEXITCODE | Should -Be 0
+    $out.results[0].result.actualState.output | Should -BeExactly 'myOutput is: Hello world!, myObject is: baz'
   }
 
   It 'Duplicated variable takes last value' {
-      $configYaml = @'
+    $configYaml = @'
 $schema: https://raw.githubusercontent.com/PowerShell/DSC/main/schemas/2024/04/config/document.json
 variables:
-myVariable: foo
-myVariable: bar
+  myVariable: foo
+  myVariable: bar
 resources:
 - name: test
-type: Microsoft.DSC.Debug/Echo
-properties:
-  output: "[variables('myVariable')]"
+  type: Microsoft.DSC.Debug/Echo
+  properties:
+    output: "[variables('myVariable')]"
 '@
-      $out = dsc config get -i $configYaml | ConvertFrom-Json
-      Write-Verbose -Verbose $out
-      $LASTEXITCODE | Should -Be 0
-      $out.results[0].result.actualState.output | Should -Be 'bar'
+    $out = dsc config get -i $configYaml | ConvertFrom-Json
+    Write-Verbose -Verbose $out
+    $LASTEXITCODE | Should -Be 0
+    $out.results[0].result.actualState.output | Should -Be 'bar'
   }
 
   It 'Missing variable returns error' {
-      $configYaml = @'
+    $configYaml = @'
 $schema: https://raw.githubusercontent.com/PowerShell/DSC/main/schemas/2024/04/config/document.json
 variables:
-hello: world
+  hello: world
 resources:
 - name: test
-type: Microsoft.DSC.Debug/Echo
-properties:
-  output: "[variables('myVariable')]"
+  type: Microsoft.DSC.Debug/Echo
+  properties:
+    output: "[variables('myVariable')]"
 '@
-      $out = dsc config get -i $configYaml 2>&1 | Out-String
-      Write-Verbose -Verbose $out
-      $LASTEXITCODE | Should -Be 2
-      $out | Should -BeLike "*Variable 'myVariable' does not exist or has not been initialized yet*"
+    $out = dsc config get -i $configYaml 2>&1 | Out-String
+    Write-Verbose -Verbose $out
+    $LASTEXITCODE | Should -Be 2
+    $out | Should -BeLike "*Variable 'myVariable' does not exist or has not been initialized yet*"
   }
 }
