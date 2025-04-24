cf87e6302e8d4af9fc272a5d417baf76a831a592
Steve Lee <slee@microsoft.com>
2025-04-23 18:01:27 -0700

    add tests

--- dsc/tests/dsc_extension_discover.ps1
+++ dsc/tests/dsc_extension_discover.ps1
@@ -21,4 +21,33 @@ Describe 'Discover extension tests' {
         $out.capabilities | Should -BeExactly @('discover')
         $out.manifest | Should -Not -BeNullOrEmpty
     }
+
+    It 'Filtering works for extension discovered resources' {
+        $out = dsc resource list '*Discovered*' | ConvertFrom-Json
+        $LASTEXITCODE | Should -Be 0
+        $out.Count | Should -Be 2
+        $out[0].type | Should -Be 'Test/DiscoveredOne'
+        $out[1].type | Should -Be 'Test/DiscoveredTwo'
+        $out[0].kind | Should -Be 'Resource'
+        $out[1].kind | Should -Be 'Resource'
+    }
+
+    It 'Extension resources can be used in config' {
+        $config_yaml = @"
+            `$schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
+            contentVersion: 1.0.0.0
+            resources:
+            - name: One
+              type: Test/DiscoveredOne
+            - name: Two
+              type: Test/DiscoveredTwo
+"@
+        $out = dsc config get -i $config_yaml | ConvertFrom-Json
+        $LASTEXITCODE | Should -Be 0
+        $out.results.Count | Should -Be 2
+        $out.results[0].type | Should -BeExactly 'Test/DiscoveredOne'
+        $out.results[0].result.actualState.Output | Should -BeExactly 'Hello One'
+        $out.results[1].type | Should -BeExactly 'Test/DiscoveredTwo'
+        $out.results[1].result.actualState.Output | Should -BeExactly 'Hello Two'
+    }
 }
