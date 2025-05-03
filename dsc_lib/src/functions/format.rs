075e6ed6a6abccba3e12cbc37bc0076c103680a9
Steve Lee <slee@microsoft.com>
2025-05-02 22:17:55 -0700

    fix function, add tests

--- dsc_lib/src/functions/format.rs
+++ dsc_lib/src/functions/format.rs
@@ -4,54 +4,76 @@
 use crate::DscError;
 use crate::configure::context::Context;
 use crate::functions::{AcceptedArgKind, Function};
-use rt_format::{Format as RtFormat, FormatArgument, ParsedFormat, Specifier};
+use rt_format::{Format as RtFormat, FormatArgument, ParsedFormat, argument::NoNamedArguments};
 use rust_i18n::t;
 use serde_json::Value;
-use std::fmt;
-use std::fmt::{Error, Formatter, Write};
 
-impl FormatArgument for Value {
-    fn supports_format(&self, spec: &Specifier) -> bool {
+#[derive(Debug, PartialEq)]
+enum Variant {
+    Boolean(bool),
+    Number(i64),
+    String(String),
+}
+
+impl FormatArgument for Variant {
+    fn supports_format(&self, specifier: &rt_format::Specifier) -> bool {
         match self {
-            Value::Boolean(_) | Value::String(_) | Value::Number(_) => true,
-            _ => false,
+            Variant::Boolean(_) | Variant::String(_)=> matches!(specifier.format, RtFormat::Display),
+            Variant::Number(_) => matches!(specifier.format, RtFormat::Display | RtFormat::Binary | RtFormat::Octal | RtFormat::LowerHex | RtFormat::UpperHex | RtFormat::Debug | RtFormat::LowerExp | RtFormat::UpperExp),
         }
     }
 
-    fn fmt_display(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt_display(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Value::Boolean(b) => write!(f, "{}", b),
-            Value::String(s) => write!(f, "{}", s),
-            Value::Number(n) => write!(f, "{}", n),
-            _ => Err(fmt::Error),
+            Variant::Boolean(value) => write!(f, "{value}"),
+            Variant::Number(value) => write!(f, "{value}"),
+            Variant::String(value) => write!(f, "{value}"),
         }
     }
 
-    fn fmt_lower_hex(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt_octal(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Value::Number(n) => write!(f, "{:x}", n.as_i64().unwrap_or_default()),
-            _ => Err(fmt::Error),
+            Variant::Number(value) => write!(f, "{value:o}"),
+            _ => Err(std::fmt::Error),
         }
     }
 
-    fn fmt_upper_hex(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt_lower_hex(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Value::Number(n) => write!(f, "{:X}", n.as_i64().unwrap_or_default()),
-            _ => Err(fmt::Error),
+            Variant::Number(value) => write!(f, "{value:x}"),
+            _ => Err(std::fmt::Error),
         }
     }
 
-    fn fmt_binary(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt_upper_hex(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Value::Number(n) => write!(f, "{:b}", n.as_i64().unwrap_or_default()),
-            _ => Err(fmt::Error),
+            Variant::Number(value) => write!(f, "{value:X}"),
+            _ => Err(std::fmt::Error),
         }
     }
 
-    fn fmt_octal(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt_binary(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Value::Number(n) => write!(f, "{:o}", n.as_i64().unwrap_or_default()),
-            _ => Err(fmt::Error),
+            Variant::Number(value) => write!(f, "{value:b}"),
+            _ => Err(std::fmt::Error),
+        }
+    }
+
+    fn fmt_debug(&self, _f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        Err(std::fmt::Error)
+    }
+
+    fn fmt_lower_exp(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Variant::Number(value) => write!(f, "{value:e}"),
+            _ => Err(std::fmt::Error),
+        }
+    }
+
+    fn fmt_upper_exp(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Variant::Number(value) => write!(f, "{value:E}"),
+            _ => Err(std::fmt::Error),
         }
     }
 }
@@ -69,29 +91,33 @@ impl Function for Format {
     }
 
     fn accepted_arg_types(&self) -> Vec<AcceptedArgKind> {
-        vec![AcceptedArgKind::Boolean, AcceptedArgKind::String, AcceptedArgKind::Number]
+        vec![AcceptedArgKind::Boolean, AcceptedArgKind::Number, AcceptedArgKind::String]
     }
 
     fn invoke(&self, args: &[Value], _context: &Context) -> Result<Value, DscError> {
-        let mut string_result = String::new();
-        let Ok(format_string) = args[0].as_str() else {
-            return Err(DscError::Parser("First `format()` argument must be a string".to_string()));
+        let Some(format_string) = args[0].as_str() else {
+            return Err(DscError::Parser(t!("functions.format.formatInvalid").to_string()));
         };
+        let mut position_args =     Vec::new();
         for value in &args[1..] {
-            if let Some(parsed_format) = ParsedFormat::parse(format_string) {
-                let mut formatted_string = String::new();
-                for specifier in parsed_format.specifiers() {
-                    if let Some(arg) = args.get(specifier.index()) {
-                        formatted_string.push_str(&arg.to_string());
+            let arg = match value {
+                Value::Bool(b) => Variant::Boolean(*b),
+                Value::Number(n) => {
+                    if let Some(i) = n.as_i64() {
+                        Variant::Number(i)
                     } else {
-                        return Err(DscError::Parser("Invalid format specifier".to_string()));
+                        return Err(DscError::Parser(t!("functions.format.numberTooLarge").to_string()));
                     }
                 }
-                string_result.push_str(&formatted_string);
-            } else {
-                return Err(DscError::Parser("Invalid format string".to_string()));
-            }
+                Value::String(s) => Variant::String(s.clone()),
+                _ => return Err(DscError::Parser(t!("functions.format.invalidArgType").to_string())),
+            };
+            position_args.push(arg);
         }
+        let string_result = match ParsedFormat::parse(format_string, &position_args, &NoNamedArguments) {
+            Ok(parsed_format) => format!("{parsed_format}"),
+            Err(_e) => return Err(DscError::Parser(t!("functions.format.invalidFormatString").to_string())),
+        };
         Ok(Value::String(string_result))
     }
 }
@@ -104,15 +130,154 @@ mod tests {
     #[test]
     fn position() {
         let mut parser = Statement::new().unwrap();
-        let result = parser.parse_and_execute("[format('{0} - {1}', 'hello', 2)]", &Context::new()).unwrap();
-        assert_eq!(result, "hello - 2");
+        let result = parser.parse_and_execute("[format('world {0} - {1}', 'hello', 2)]", &Context::new()).unwrap();
+        assert_eq!(result, "world hello - 2");
+    }
+
+    #[test]
+    fn reverse_position() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('two{1} - {0}world', 'hello', 2)]", &Context::new()).unwrap();
+        assert_eq!(result, "two2 - helloworld");
+    }
+
+    #[test]
+    fn repeated_position() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0} - {0}{1}', 'hello', 2)]", &Context::new()).unwrap();
+        assert_eq!(result, "hello - hello2");
     }
 
     #[test]
     fn numbers_as_hex() {
         let mut parser = Statement::new().unwrap();
-        let result = parser.parse_and_execute("[format('{0:x} {0:X}', 12, 13)]", &Context::new()).unwrap();
-        assert_eq!(result, "c D");
+        let result = parser.parse_and_execute("[format('{0:x} = {1:X}', 12, 13)]", &Context::new()).unwrap();
+        assert_eq!(result, "c = D");
     }
 
+    #[test]
+    fn numbers_as_octal() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0:o} == {1:o}', 12, 13)]", &Context::new()).unwrap();
+        assert_eq!(result, "14 == 15");
+    }
+
+    #[test]
+    fn numbers_as_binary() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0:b} = {1:b}', 12, 13)]", &Context::new()).unwrap();
+        assert_eq!(result, "1100 = 1101");
+    }
+
+    #[test]
+    fn numbers_as_exp() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0:e} = {1:E}', 12, 13)]", &Context::new()).unwrap();
+        assert_eq!(result, "1.2e1 = 1.3E1");
+    }
+
+    #[test]
+    fn numbers_as_display_just_one() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('hello {0} there', 12, 13)]", &Context::new()).unwrap();
+        assert_eq!(result, "hello 12 there");
+    }
+
+    #[test]
+    fn string_as_octal() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0:o} = {1:O}', 'hello', 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn bool_as_octal() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0:o} = {1:O}', true, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn string_as_hex() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0:x} = {1:X}', 'hello', 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn bool_as_hex() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{0:x} = {1:X}', true, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn string_as_binary() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{1:b} = {0:B}', 'hello', 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn bool_as_binary() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{1:b} = {0:B}', true, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn string_as_exp() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{1:e} = {0:E}', 'hello', 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn bool_as_exp() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('{1:e} = {0:E}', true, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn args_out_of_bounds() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('hello {1} {2} there', 12, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn missing_closing_brace() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('hello {0 there', 12, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn missing_opening_brace() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('hello 0} there', 12, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn invalid_format_option() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('hello {0:invalid} there', 12, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn invalid_index_syntax() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('hello {0;x} there', 12, 13)]", &Context::new());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn missing_format_type() {
+        let mut parser = Statement::new().unwrap();
+        let result = parser.parse_and_execute("[format('hello {0:} there', 12, 13)]", &Context::new()).unwrap();
+        assert_eq!(result, "hello 12 there");
+    }
 }
