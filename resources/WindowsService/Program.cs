b6d3a327e7878f4a41589f078d94a6ccb3ed604d
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2025-05-08 15:01:07 -0700

    Step 3: Serialize to JSON

--- resources/WindowsService/Program.cs
+++ resources/WindowsService/Program.cs
@@ -1,10 +1,36 @@
 ﻿using System.ServiceProcess;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var services = ServiceController.GetServices();
+var jsonSerializerOptions = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    Converters =
+    {
+        new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+    }
+};
+
 foreach (var service in services)
 {
-    Console.WriteLine($"Service Name: {service.ServiceName}");
-    Console.WriteLine($"Display Name: {service.DisplayName}");
-    Console.WriteLine($"Status: {service.Status}");
-    Console.WriteLine($"Start Type: {service.StartType}\n");
+    var windowsService = new WindowsService
+    {
+        ServiceName = service.ServiceName,
+        DisplayName = service.DisplayName,
+        Status = service.Status,
+        StartType = service.StartType
+    };
+
+
+    string json = JsonSerializer.Serialize(windowsService, jsonSerializerOptions);
+    Console.WriteLine(json);
+}
+
+class WindowsService
+{
+    public string ServiceName { get; set; }
+    public string DisplayName { get; set; }
+    public ServiceControllerStatus Status { get; set; }
+    public ServiceStartMode StartType { get; set; }
 }
