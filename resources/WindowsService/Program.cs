6356b8eb5e003b2f564845573e04710b28f5b3af
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2025-05-08 14:50:50 -0700

    Step 2: Enumerate Windows services

--- resources/WindowsService/Program.cs
+++ resources/WindowsService/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ServiceProcess;
+
+var services = ServiceController.GetServices();
+foreach (var service in services)
+{
+    Console.WriteLine($"Service Name: {service.ServiceName}");
+    Console.WriteLine($"Display Name: {service.DisplayName}");
+    Console.WriteLine($"Status: {service.Status}");
+    Console.WriteLine($"Start Type: {service.StartType}\n");
+}
