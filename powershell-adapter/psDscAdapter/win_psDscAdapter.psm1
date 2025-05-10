4db895ee46e6104175937ea077aaf44b88e69e31
G.Reijn <gijsreijn@hotmail.com>
2025-05-10 08:08:47 +0200

    Fix remarks

--- powershell-adapter/psDscAdapter/win_psDscAdapter.psm1
+++ powershell-adapter/psDscAdapter/win_psDscAdapter.psm1
@@ -224,7 +224,7 @@ function Invoke-DscCacheRefresh {
             }
 
             # workaround: Use GetTypeInstanceFromModule to get the type instance from the module and validate if it is a class-based resource
-            $classBased = GetTypeInstanceFromModule -modulename $moduleName -classname $dscResource.Name -ErrorAction SilentlyContinue
+            $classBased = GetTypeInstanceFromModule -modulename $moduleName -classname $dscResource.Name -ErrorAction Ignore
             if (-not ([string]::IsNullOrEmpty($classBased))) {
                 $dscResourceInfo.ImplementationDetail = 'ClassBased'
             }
