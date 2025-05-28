ef80dae925cb0f654b5600e0e4a767405d2aba61
G.Reijn <gijsreijn@hotmail.com>
2025-05-28 16:15:25 +0200

    WhatIf on PowerShell adapter

--- build.ps1
+++ build.ps1
@@ -518,7 +518,7 @@ if ($Test) {
         (Get-Module -Name Pester -ListAvailable).Path
     }
 
-    Invoke-Pester -ErrorAction Stop -Output Detailed
+    Invoke-Pester -ErrorAction Stop
 }
 
 function Find-MakeAppx() {
