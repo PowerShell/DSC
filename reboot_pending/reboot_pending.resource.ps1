2f19a055d2b11beefd003a3662ff07d728d883c0
G.Reijn <gijsreijn@hotmail.com>
2025-06-11 05:46:22 +0200

    Resolve remarks

--- reboot_pending/reboot_pending.resource.ps1
+++ reboot_pending/reboot_pending.resource.ps1
@@ -1,19 +1,22 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 # Reg keys are documented here: https://learn.microsoft.com/en-us/mem/configmgr/core/servers/deploy/install/list-of-prerequisite-checks#pending-system-restart
-$reasons = @()
+$reasons = [System.Collections.Generic.List[string[]]]::new()
 if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { 
-  $reasons += "Component Based Servicing"
+  $reasons.Add("Component Based Servicing")
 }
 if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { 
-  $reasons += "Windows Update"
+  $reasons.Add("Windows Update")
 }
 if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { 
-  $reasons += "Pending File Rename Operations"
+  $reasons.Add("Pending File Rename Operations")
 }
 try { 
   $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
   $status = $util.DetermineIfRebootPending()
   if(($null -ne $status) -and $status.RebootPending){
-    $reasons += "SCCM Client"
+    $reasons.Add("SCCM Client")
   }
 }catch{}
 
@@ -21,4 +24,5 @@ $result = @{
   rebootPending = $reasons.Count -gt 0
   reason = if ($reasons.Count -gt 0) { $reasons } else { $null }
 }
+
 return $result | ConvertTo-Json -Compress
