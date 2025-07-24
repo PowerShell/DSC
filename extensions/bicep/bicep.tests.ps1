3af52aefbea1d1f879e8840ff3471fa14c01a4c5
Steve Lee <slee@microsoft.com>
2025-07-23 19:27:15 -0700

    i18n fix

--- extensions/bicep/bicep.tests.ps1
+++ extensions/bicep/bicep.tests.ps1
@@ -12,7 +12,7 @@ BeforeDiscovery {
 Describe 'Bicep extension tests' -Skip:(!$foundBicep) {
     It 'Example bicep file should work' {
         $bicepFile = Resolve-Path -Path "$PSScriptRoot\..\..\dsc\examples\hello_world.dsc.bicep"
-        $out = dsc -l debug config get -f $bicepFile 2>$TestDrive/error.log | ConvertFrom-Json
+        $out = dsc -l trace config get -f $bicepFile 2>$TestDrive/error.log | ConvertFrom-Json
         $LASTEXITCODE | Should -Be 0 -Because (Get-Content -Path $TestDrive/error.log -Raw | Out-String)
         $out.results[0].result.actualState.output | Should -BeExactly 'Hello, world!'
         (Get-Content -Path $TestDrive/error.log -Raw) | Should -Match "Importing file '$bicepFile' with extension 'Microsoft.DSC.Extension/Bicep'"
