ccb5093019fa850af994d7edf7639dc70ce7d925
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2025-07-29 18:59:17 -0700

    fix reboot pending test

--- reboot_pending/tests/reboot_pending.tests.ps1
+++ reboot_pending/tests/reboot_pending.tests.ps1
@@ -9,33 +9,35 @@ BeforeDiscovery {
     }
 }
 
-Describe 'reboot_pending resource tests' {
-    It 'should get reboot_pending' -Skip:(!$IsWindows) {
+Describe 'reboot_pending resource tests' -Skip:(!$IsWindows -or !$isElevated) {
+    BeforeAll {
+        $keyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"
+        $keyName = "RebootRequired"
+        if (-not (Get-ItemProperty "$keyPath\$keyName" -ErrorAction SilentlyContinue)) {
+            New-ItemProperty -Path $keyPath -Name $keyName -Value 1 -PropertyType DWord -Force | Out-Null
+        }
+    }
+
+    AfterAll {
+        Remove-ItemProperty -Path $keyPath -Name $keyName -ErrorAction Ignore
+    }
+
+    It 'should get reboot_pending' {
         $out = dsc resource get -r Microsoft.Windows/RebootPending | ConvertFrom-Json
         $LASTEXITCODE | Should -Be 0
         $out.actualState.rebootPending | Should -Not -BeNullOrEmpty
     }
 
-    It 'reboot_pending works in a config' -Skip:(!$IsWindows) {
+    It 'reboot_pending works in a config' {
         $ConfigPath = Resolve-Path "$PSScriptRoot/reboot_pending.dsc.yaml"
         $out = dsc config get --file $ConfigPath | ConvertFrom-Json
         $LASTEXITCODE | Should -Be 0
         $out.results.result.actualState.rebootPending | Should -Not -BeNullOrEmpty
     }
 
-    It 'reboot_pending should have a reason' -Skip:(!$IsWindows -or !$isElevated) {
-        $keyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"
-        $keyName = "RebootRequired"
-        try {
-            if (-not (Get-ItemProperty "$keyPath\$keyName" -ErrorAction SilentlyContinue)) {
-                New-ItemProperty -Path $keyPath -Name $keyName -Value 1 -PropertyType DWord -Force | Out-Null
-            }
-
-            $out = dsc resource get -r Microsoft.Windows/RebootPending | ConvertFrom-Json
-            $LASTEXITCODE | Should -Be 0
-            $out.actualState.reason.count | Should -BeGreaterThan 0 -Because ($out | Out-String)
-        } finally {
-            Remove-ItemProperty -Path $keyPath -Name $keyName -ErrorAction Ignore
-        }
+    It 'reboot_pending should have a reason' {
+        $out = dsc resource get -r Microsoft.Windows/RebootPending | ConvertFrom-Json
+        $LASTEXITCODE | Should -Be 0
+        $out.actualState.reason.count | Should -BeGreaterThan 0 -Because ($out | Out-String)
     }
 }
