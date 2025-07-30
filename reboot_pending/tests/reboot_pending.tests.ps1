9de0cad561a9cf3a95a945bf47b4ba26f72407d5
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2025-07-29 17:07:55 -0700

    fix tests

--- reboot_pending/tests/reboot_pending.tests.ps1
+++ reboot_pending/tests/reboot_pending.tests.ps1
@@ -1,6 +1,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+BeforeDiscovery {
+    if ($IsWindows) {
+        $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $principal = [System.Security.Principal.WindowsPrincipal]::new($identity)
+        $isElevated = $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+}
+
 Describe 'reboot_pending resource tests' {
     It 'should get reboot_pending' -Skip:(!$IsWindows) {
         $out = dsc resource get -r Microsoft.Windows/RebootPending | ConvertFrom-Json
@@ -15,7 +23,7 @@ Describe 'reboot_pending resource tests' {
         $out.results.result.actualState.rebootPending | Should -Not -BeNullOrEmpty
     }
 
-    It 'reboot_pending should have a reason' -Skip:(!$IsWindows) {
+    It 'reboot_pending should have a reason' -Skip:(!$IsWindows -or !$isElevated) {
         $keyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"
         $keyName = "RebootRequired"
         try {
@@ -23,9 +31,9 @@ Describe 'reboot_pending resource tests' {
                 New-ItemProperty -Path $keyPath -Name $keyName -Value 1 -PropertyType DWord -Force | Out-Null
             }
 
-            $out | dsc resource get -r Microsoft.Windows/RebootPending | ConvertFrom-Json
+            $out = dsc resource get -r Microsoft.Windows/RebootPending | ConvertFrom-Json
             $LASTEXITCODE | Should -Be 0
-            $out.actualState.reason.count | Should -BeGreaterThan 0
+            $out.actualState.reason.count | Should -BeGreaterThan 0 -Because ($out | Out-String)
         } finally {
             Remove-ItemProperty -Path $keyPath -Name $keyName -ErrorAction Ignore
         }
