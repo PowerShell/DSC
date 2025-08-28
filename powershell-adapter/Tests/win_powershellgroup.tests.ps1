6f92b4655003271c1a0a594f592348334a39d2e6
Steve Lee <slee@microsoft.com>
2025-08-28 11:07:05 -0700

    fix test

--- powershell-adapter/Tests/win_powershellgroup.tests.ps1
+++ powershell-adapter/Tests/win_powershellgroup.tests.ps1
@@ -218,7 +218,7 @@ resources:
   }
 
   It 'Set works with class-based PS DSC resources' {
-    $out = dsc resource set -r PSClassResource/PSClassResource --input (@{Name = 'TestName' } | ConvertTo-Json) 2> "$testdrive/error.log" | ConvertFrom-Json
+    $out = dsc resource set -r PSClassResource/PSClassResource --input (@{Name = 'TestName'; Credential = @{"UserName" = "MyUser"; "Password" = "MyPassword"} } | ConvertTo-Json) 2> "$testdrive/error.log" | ConvertFrom-Json
     $LASTEXITCODE | Should -Be 0 -Because (Get-Content -Path "$testdrive/error.log" -Raw | Out-String)
     $out.afterstate.InDesiredState | Should -Be $true
   }
