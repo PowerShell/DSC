681cb24524c269e770f6d6305f413b6be1a8692d
G.Reijn <gijsreijn@hotmail.com>
2025-08-31 05:56:50 +0200

    Format

--- dsc/tests/dsc_version.tests.ps1
+++ dsc/tests/dsc_version.tests.ps1
@@ -2,8 +2,8 @@
 # Licensed under the MIT License.
 
 Describe 'tests for metadata versioning' {
-  It 'returns the correct dsc semantic version in metadata' {
-    $config_yaml = @"
+    It 'returns the correct dsc semantic version in metadata' {
+        $config_yaml = @"
             `$schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
             resources:
             - name: Echo
@@ -11,10 +11,10 @@ Describe 'tests for metadata versioning' {
               properties:
                 output: 'Hello, World!'
 "@
-    $out = $config_yaml | dsc config get -f - | ConvertFrom-Json
-    $version = $out.metadata.'Microsoft.DSC'.version -as [System.Management.Automation.SemanticVersion]
-    $version | Should -Not -BeNullOrEmpty
-    $dscVersion = (dsc --version).Split(" ")[1]
-    $version | Should -Be $dscVersion
-  }
+        $out = $config_yaml | dsc config get -f - | ConvertFrom-Json
+        $version = $out.metadata.'Microsoft.DSC'.version -as [System.Management.Automation.SemanticVersion]
+        $version | Should -Not -BeNullOrEmpty
+        $dscVersion = (dsc --version).Split(" ")[1]
+        $version | Should -Be $dscVersion
+    }
 }
