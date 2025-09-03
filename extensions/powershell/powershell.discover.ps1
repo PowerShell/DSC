a7263ba067eaf319ffc5533d3808cade4b2e8287
G.Reijn <gijsreijn@hotmail.com>
2025-09-03 19:20:02 +0200

    Fix point 2

--- extensions/powershell/powershell.discover.ps1
+++ extensions/powershell/powershell.discover.ps1
@@ -1,18 +1,25 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-$psPaths = $env:PSModulePath -split [System.IO.Path]::PathSeparator | Where-Object { $_ -notmatch 'WindowsPowerShell' }
+[CmdletBinding()]
+param ()
 
-$manifests = $psPaths | ForEach-Object -Parallel {
-    $searchPatterns = @('*.dsc.resource.json', '*.dsc.resource.yaml', '*.dsc.resource.yml')
-    $enumOptions = [System.IO.EnumerationOptions]@{ IgnoreInaccessible = $false; RecurseSubdirectories = $true }
-    foreach ($pattern in $searchPatterns) {
-        try {
-            [System.IO.Directory]::EnumerateFiles($_, $pattern, $enumOptions) | ForEach-Object {
-                @{ manifestPath = $_ }
-            }
-        } catch { }
-    }
-} -ThrottleLimit 10
-
-$manifests | ForEach-Object { $_ | ConvertTo-Json -Compress }
+begin {
+    $psPaths = $env:PSModulePath -split [System.IO.Path]::PathSeparator | Where-Object { $_ -notmatch 'WindowsPowerShell' }
+}
+process {
+    $manifests = $psPaths | ForEach-Object -Parallel {
+        $searchPatterns = @('*.dsc.resource.json', '*.dsc.resource.yaml', '*.dsc.resource.yml')
+        $enumOptions = [System.IO.EnumerationOptions]@{ IgnoreInaccessible = $false; RecurseSubdirectories = $true }
+        foreach ($pattern in $searchPatterns) {
+            try {
+                [System.IO.Directory]::EnumerateFiles($_, $pattern, $enumOptions) | ForEach-Object {
+                    @{ manifestPath = $_ }
+                }
+            } catch { }
+        }
+    } -ThrottleLimit 10
+}
+end {
+    $manifests | ForEach-Object { $_ | ConvertTo-Json -Compress }
+}
