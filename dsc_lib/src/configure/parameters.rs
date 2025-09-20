10ecdc8d5ab2fd564807a0d40ce043193b321bb8
Steve Lee <slee@microsoft.com>
2025-09-20 13:31:05 -0700

    Implement Display for secure types

--- dsc_lib/src/configure/parameters.rs
+++ dsc_lib/src/configure/parameters.rs
@@ -4,7 +4,7 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Display};
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, JsonSchema)]
 pub struct Input {
@@ -18,4 +18,13 @@ pub enum SecureKind {
     SecureString(String),
     #[serde(rename = "secureObject")]
     SecureObject(Value),
+}
+
+impl Display for SecureKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SecureKind::SecureString(_) => write!(f, "<secureString>"),
+            SecureKind::SecureObject(_) => write!(f, "<secureObject>"),
+        }
+    }
 }
