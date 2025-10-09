12f6f91a6d3d6028cb012f95bf5a0c7fbc971a08
Mikey Lombardi <mlombardi@microsoft.com>
2025-10-09 15:28:06 -0500

    (GH-538) Add `dsc-lib-jsonschema` crate
    This change begins the schema canonicalization process by
    adding a new library crate to contain the helpers for
    defining canonical and enhanced schemas ergonomically.
    
    In this initial phase, the new crate defines:
    
    - The `VSCODE_KEYWORDS` constant, enumerating the keywords
      that VS Code's JSON language server recognizes.
    - The `idiomaticize_string_enum` transformer for munging the
      generated schemas for string enums with annotation keywords.
    - The `idiomaticize_externally_tagged_enum` transformer for
      munging the generated schemas for externally tagged enums
      to a more idiomatic and readable representation.

---
+++ lib/dsc-lib-jsonschema/src/tests/mod.rs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! Defines unit tests for [`dsc-lib-jsonschema`].
+//!
+//! Instead of defining tests in each of the module files for the crate, we
+//! define them in this module to improve compilation times. The tests in this
+//! module are for internal code. Do not define tests for public items in this
+//! module. Instead, define those tests in the `tests/integration` folder,
+//! which forces usage of the crate as a public API.
+//! 
+//! When you define tests in this module, ensure that you mirror the structure
+//! of the modules from the rest of the source tree.
+
+#[cfg(test)] mod transforms;
+#[cfg(test)] mod vscode;
