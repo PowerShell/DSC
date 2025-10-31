de4cd2cb67426ba15a91fbe84370b6d644f94cc3
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2025-10-31 12:46:47 -0700

    clean up code, add negative test

--- dsc/tests/dsc_discovery.tests.ps1
+++ dsc/tests/dsc_discovery.tests.ps1
@@ -246,15 +246,19 @@ Describe 'tests for resource discovery' {
         }
     }
 
-    It 'Resource manifest using relative path to exe works' {
-        $manifest = @'
+    It 'Resource manifest using relative path to exe: <path>' -TestCases @(
+        @{ path = '../dscecho'; success = $true }
+        @{ path = '../foo/dscecho'; success = $false }
+    ) {
+        param($path, $success)
+        $manifest = @"
 {
-    "$schema": "https://aka.ms/dsc/schemas/v3/bundled/resource/manifest.json",
+    "`$schema": "https://aka.ms/dsc/schemas/v3/bundled/resource/manifest.json",
     "type": "Microsoft.DSC.Debug/Echo",
     "version": "1.0.0",
     "description": "Echo resource for testing and debugging purposes",
     "get": {
-        "executable": "../dscecho",
+        "executable": "$path",
         "args": [
             {
                 "jsonInputArg": "--input",
@@ -264,24 +268,29 @@ Describe 'tests for resource discovery' {
     },
     "schema": {
         "command": {
-            "executable": "../dscecho"
+            "executable": "$path"
         }
     }
 }
-'@
+"@
         $dscEcho = Get-Command dscecho -ErrorAction Stop
         # copy to testdrive
         Copy-Item -Path "$($dscEcho.Source)" -Destination $testdrive
         # create manifest in subfolder
         $subfolder = Join-Path $testdrive 'subfolder'
-        New-Item -Path $subfolder -ItemType Directory | Out-Null
+        New-Item -Path $subfolder -ItemType Directory -Force | Out-Null
         Set-Content -Path (Join-Path $subfolder 'test.dsc.resource.json') -Value $manifest
 
         try {
             $env:DSC_RESOURCE_PATH = $subfolder
             $out = dsc resource get -r 'Microsoft.DSC.Debug/Echo' -i '{"output":"RelativePathTest"}' 2> "$testdrive/error.txt" | ConvertFrom-Json
-            $LASTEXITCODE | Should -Be 0 -Because (Get-Content -Raw -Path "$testdrive/error.txt")
-            $out.actualState.output | Should -BeExactly 'RelativePathTest'
+            if ($success) {
+                $LASTEXITCODE | Should -Be 0 -Because (Get-Content -Raw -Path "$testdrive/error.txt")
+                $out.actualState.output | Should -BeExactly 'RelativePathTest'
+            } else {
+                $LASTEXITCODE | Should -Be 2 -Because (Get-Content -Raw -Path "$testdrive/error.txt")
+                (Get-Content -Raw -Path "$testdrive/error.txt") | Should -Match "ERROR.*?Executable '\.\./foo/dscecho(\.exe)?' not found"
+            }
         }
         finally {
             $env:DSC_RESOURCE_PATH = $null
