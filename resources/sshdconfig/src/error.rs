31dc767a0d8e87743c11177532fcf272919c7730
Tess Gauthier <tessgauthier@microsoft.com>
2025-12-04 11:40:38 -0500

    fix error order

--- resources/sshdconfig/src/error.rs
+++ resources/sshdconfig/src/error.rs
@@ -9,10 +9,10 @@ use thiserror::Error;
 pub enum SshdConfigError {
     #[error("{t}: {0}", t = t!("error.command"))]
     CommandError(String),
-    #[error("{t}: {0}", t = t!("error.fmt"))]
-    FmtError(#[from] std::fmt::Error),
     #[error("{t}: {0}", t = t!("error.envVar"))]
     EnvVarError(#[from] std::env::VarError),
+    #[error("{t}: {0}", t = t!("error.fmt"))]
+    FmtError(#[from] std::fmt::Error),
     #[error("{t}: {0}", t = t!("error.invalidInput"))]
     InvalidInput(String),
     #[error("{t}: {0}", t = t!("error.io"))]
