fb4fb26a3c1bd4b8306b7eb3e9d1f62df1a9e1e6
Mikey Lombardi <mlombardi@microsoft.com>
2025-12-04 13:57:34 -0600

    (GH-538) Re-export `dsc-lib-jsonschema` in `dsc-lib`
    Prior to this change, the `dsc-lib` crate used `dsc-lib-jsonschema` as
    a dependency. With the transition of the `DscRepoSchema` trait and the
    related types to the `dsc-lib-jsonschema` crate, it makes more sense to
    re-export the dependency from within the `dsc-lib` crate.
    
    This change re-exports the `dsc-lib-jsonschema` crate as
    `dsc_lib::schemas`, replacing the prior module and direct references to
    `dsc-lib-jsonschema` for transformers and other crate functionality.
    
    Now other crates can choose whether to depend on `dsc-lib` with all of
    the associated functionality, or `dsc-lib-jsonschema` if they only need
    the JSON Schema related functionality.

--- lib/dsc-lib/src/discovery/command_discovery.rs
+++ lib/dsc-lib/src/discovery/command_discovery.rs
@@ -12,7 +12,7 @@ use crate::extensions::dscextension::{self, DscExtension, Capability as Extensio
 use crate::extensions::extension_manifest::ExtensionManifest;
 use crate::progress::{ProgressBar, ProgressFormat};
 use crate::util::convert_wildcard_to_regex;
-use dsc_lib_jsonschema::transforms::idiomaticize_externally_tagged_enum;
+use crate::schemas::transforms::idiomaticize_externally_tagged_enum;
 use regex::RegexBuilder;
 use rust_i18n::t;
 use semver::{Version, VersionReq};
