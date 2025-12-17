169eeb62d1769d5e9d7bd3900897d6026a61a867
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-12-16 19:11:25 -0800

    Don't build gRPC client

--- dscbicep/build.rs
+++ dscbicep/build.rs
@@ -2,6 +2,10 @@
 // Licensed under the MIT License.
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/bicep.proto")?;
+    tonic_prost_build::configure()
+    .build_client(false)
+    // TODO: Configure and commit the out_dir to avoid dependency on protoc
+    // .out_dir(out_dir)
+    .compile_protos(&["proto/bicep.proto"], &["proto"])?;
     Ok(())
 }
