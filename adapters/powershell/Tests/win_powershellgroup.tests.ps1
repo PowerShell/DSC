2d10057c93d615ec24372c0e753b8bd947487950
Michal Machniak <mmachniak@outlook.com>
2026-01-15 12:57:37 +0100

    Add to unit test for PowerShell adapter in verions 5.1

--- adapters/powershell/Tests/win_powershellgroup.tests.ps1
+++ adapters/powershell/Tests/win_powershellgroup.tests.ps1
@@ -118,6 +118,207 @@ class PSClassResource {
       New-Item -Path $modulePath -ItemType File -Value $module -Force | Out-Null
     }
 
+    ## Add script base Classs for testing
+
+    $moduleFileScriptRootPSD1 = @"
+    @{
+        # Script module or binary module file associated with this manifest.
+        RootModule            = 'TestScriptBaseDSC.psm1'
+
+        # Version number of this module.
+        moduleVersion        = '0.0.1'
+
+        # ID used to uniquely identify this module
+        GUID                 = 'c3775be8-84a1-43f5-a99c-1b9f2d6bc178'
+
+        # Author of this module
+        Author               = ''
+
+        # Company or vendor of this module
+        CompanyName          = ''
+
+        # Copyright statement for this module
+        Copyright            = ''
+
+        # Description of the functionality provided by this module
+        Description          = ''
+
+        # Minimum version of the Windows PowerShell engine required by this module
+        PowerShellVersion    = '5.0'
+
+        # Cmdlets to export from this module
+        CmdletsToExport      = @()
+
+        # Variables to export from this module
+        VariablesToExport    = @()
+
+        # Aliases to export from this module
+        AliasesToExport      = @()
+
+        # Dsc Resources to export from this module
+        DscResourcesToExport = @('CredentialValidation')
+
+        # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+        PrivateData          = @{
+
+            PSData = @{
+
+                # Tags applied to this module. These help with module discovery in online galleries.
+                Tags         = @('DesiredStateConfiguration', 'DSC', 'DSCResourceKit', 'DSCResource')
+
+                # A URL to the license for this module.
+                LicenseUri   = ''
+
+                # A URL to the main website for this project.
+                ProjectUri   = ''
+
+                # A URL to an icon representing this module.
+                IconUri      = ''
+
+                # ReleaseNotes of this module
+                ReleaseNotes = ''
+
+                # Set to a prerelease string value if the release should be a prerelease.
+                Prerelease   = ''
+            } # End of PSData hashtable
+        } # End of PrivateData hashtable
+    }
+
+    "@
+    $moduleScriptRootPSM1 = @"
+    # Root module for CredentialValidationDsc
+    # No code required
+    "@
+
+    $moduleScriptCredentialValidationPSM1 = @'
+
+
+    $VerbosePreference = 'SilentlyContinue'
+    $InformationPreference = 'SilentlyContinue'
+    $ProgressPreference = 'Continue'
+    $ErrorActionPreference = 'SilentlyContinue'
+
+    function Get-TargetResource {
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
+        [OutputType([Hashtable])]
+        param (
+            [Parameter(Mandatory)]
+            [string] $Name,
+
+            [Parameter(Mandatory = $true)]
+            [System.Management.Automation.PSCredential]
+            $Credential
+        )
+        Write-Verbose "[GET] Get Function running"
+        return @{
+                Name = $Name
+                Credential = $Credential
+        }
+      
+    }
+
+    function Test-TargetResource {
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
+        [OutputType([System.Boolean])]
+        param (
+            [Parameter(Mandatory)]
+            [string] $Name,
+
+            [Parameter(Mandatory = $true)]
+            [System.Management.Automation.PSCredential]
+            $Credential
+
+        )
+        Write-Verbose "[TEST]Checking credentials"
+        Write-Verbose "[TEST]Checking credentials UserName:  $($Credential.UserName)"
+        Write-Verbose "[TEST]Checking credentials Password:  $($Credential.Password)"
+
+      if ($null -eq $Credential) {
+              throw 'Credential property is required'
+              $inDesiredState = $false
+              return $false
+            }
+
+        if ($Credential.UserName -ne 'MyUser') {
+                throw 'Invalid user name'
+                $inDesiredState = $false
+        } else {
+                $inDesiredState = $true
+        }
+
+
+        return $inDesiredState
+        
+    }
+
+    function Set-TargetResource {
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory)]
+            [string] $Name,
+
+            [Parameter(Mandatory = $true)]
+            [System.Management.Automation.PSCredential]
+            $Credential
+
+        )
+
+          if ($null -eq $Credential) {
+              throw 'Credential property is required'
+              $inDesiredState = $false
+              return $false
+            }
+
+            if ($Credential.UserName -ne 'MyUser') {
+                    throw 'Invalid user name'
+                    $inDesiredState = $false
+            } else {
+                    $inDesiredState = $true
+            }
+
+
+            return $inDesiredState
+        Write-Verbose "[SET]Credential cannot be remediated by DSC."
+    }
+
+    '@
+    $moduleScriptCredentialValidationSchemaMof = @"
+    [ClassVersion("1.0.0.0"), FriendlyName("CredentialValidation")]
+    class CredentialValidation : OMI_BaseResource
+    {
+        [Key] string Name;
+        [Required, Description("Test Credentials for Script Base"), EmbeddedInstance("MSFT_Credential")] String Credential;
+    };
+
+    "@
+
+
+
+    $modulePathRootPSM1 = Join-Path $windowsPowerShellPath 'TestScriptBaseDSC' 'TestScriptBaseDSC.psm1'
+        if (-not (Test-Path -Path $modulePathRootPSM1)) {
+        New-Item -Path $modulePathRootPSM1 -ItemType File -Value $moduleScriptRootPSM1 -Force | Out-Null
+    }
+
+
+    $modulePathRootPSD1 = Join-Path $windowsPowerShellPath 'TestScriptBaseDSC' 'TestScriptBaseDSC.psd1'
+        if (-not (Test-Path -Path $modulePathRootPSD1)) {
+        New-Item -Path $modulePathRootPSD1 -ItemType File -Value $moduleFileScriptRootPSD1 -Force | Out-Null
+    }
+
+
+    $modulePathScriptCredentialValidationPSM1 = Join-Path $windowsPowerShellPath 'TestScriptBaseDSC' 'DSCResources' 'CredentialValidation' 'CredentialValidation.psm1'
+    if (-not (Test-Path -Path $modulePathScriptCredentialValidationPSM1)) {
+        Write-Host "File will be created: $modulePathScriptCredentialValidationPSM1"
+        New-Item -Path $modulePathScriptCredentialValidationPSM1 -ItemType File -Value $moduleScriptCredentialValidationPSM1 -Force | Out-Null
+    }
+
+    $modulePathScriptCredentialValidationSchemaMof = Join-Path $windowsPowerShellPath 'TestScriptBaseDSC' 'DSCResources' 'CredentialValidation' 'CredentialValidation.schema.mof'
+    if (-not (Test-Path -Path $modulePathScriptCredentialValidationSchemaMof)) {
+        Write-Host "File will be created: $modulePathScriptCredentialValidationSchemaMof"
+        New-Item -Path $modulePathScriptCredentialValidationSchemaMof -ItemType File -Value $moduleScriptCredentialValidationSchemaMof -Force | Out-Null
+    }
+
     $env:PSModulePath = $windowsPowerShellPath + [System.IO.Path]::PathSeparator + $env:PSModulePath + [System.IO.Path]::PathSeparator
   }
 
@@ -165,20 +366,16 @@ resources:
   }
 
   It 'Config works with credential object' {
-    $yaml = @'
-    $schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
-    parameters:
-      Credential:
-      type: secureObject
-      defaultValue:
-        username: MyUser
-        password: MyPassword
-    resources:
-      - name: Cred test
-        type: PSClassResource/PSClassResource
-        properties:
-          Name: Test
-          Credential: "[parameters('Credential')]"
+  $yaml = @'
+   $schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
+   resources:
+     - name: Cred test
+       type: PSClassResource/PSClassResource
+       properties:
+         Name: Test
+         Credential:
+           UserName: 'MyUser'
+           Password: 'MyPassword'
 '@
 
     $out = dsc -l debug config set -i $yaml 2> "$testdrive/error.log" | ConvertFrom-Json
@@ -204,12 +401,13 @@ resources:
     (Get-Content -Path "$testdrive/error.log" -Raw) | Should -BeLike "*ERROR*Credential object 'Credential' requires both 'username' and 'password' properties*" -Because (Get-Content -Path "$testdrive/error.log" -Raw | Out-String)
   }
 
-  ## Scipt base resources test 
+  ## Scipt base resources test running
 
-    It 'Config works with credential object with Script base resources' {
+It 'Valide credentials with Script base resources' {
 
 $inDesiredState = $true
-$yaml =  @'
+
+$yaml = @'
 $schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
 resources:
 - name: Working with classic DSC resources
@@ -221,21 +419,20 @@ resources:
       properties:
         Name: TestScriptResource1
         Credential:       
-          username: MyUser
-          password: Password
+           username: MyUser
+           password: Password
 '@
 
-
-$out = dsc -l debug config test -i $yaml 2> "$testdrive/error.log" | ConvertFrom-Json
+$out = dsc -l trace config test -i $yaml 2>"$testdrive/error.log" | ConvertFrom-Json
 $LASTEXITCODE | Should -Be 0 -Because (Get-Content -Path "$testdrive/error.log" -Raw | Out-String)
 $out.results[0].result.inDesiredState | Should -Be $inDesiredState
-
 }
 
-  It 'Config dont works with credential object with Script base resources - wrong user' {
+It 'Not Valide credentials with Script base resources - wrong username' {
 
 $inDesiredState = $false
-$yaml =  @'
+
+$yaml = @'
 $schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
 resources:
 - name: Working with classic DSC resources
@@ -247,21 +444,19 @@ resources:
       properties:
         Name: TestScriptResource1
         Credential:       
-          username: MyUser1
-          password: Password
+           username: User
+           password: Password
 '@
 
-
-$out = dsc -l debug config test -i $yaml 2> "$testdrive/error.log" | ConvertFrom-Json
+$out = dsc -l trace config test -i $yaml 2>"$testdrive/error.log" | ConvertFrom-Json
 $LASTEXITCODE | Should -Be 0 -Because (Get-Content -Path "$testdrive/error.log" -Raw | Out-String)
 $out.results[0].result.inDesiredState | Should -Be $inDesiredState
-
 }
 
-  It 'Config dont works with credential object with Script base resources - No passowrd' {
+It 'Not Valide credentials with Script base resources - wrong properties' {
 
-$inDesiredState = $false
-$yaml =  @'
+
+$yaml = @'
 $schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
 resources:
 - name: Working with classic DSC resources
@@ -273,14 +468,14 @@ resources:
       properties:
         Name: TestScriptResource1
         Credential:       
-          username: MyUser
-          Notpassword: Password
+           username: MyUser
+           Notpassword: Password
 '@
 
-
-$out = dsc -l debug config test -i $yaml 2> "$testdrive/error.log" | ConvertFrom-Json
-$LASTEXITCODE | Should -Be 0 -Because (Get-Content -Path "$testdrive/error.log" -Raw | Out-String)
-$out.results[0].result.inDesiredState | Should -Be $inDesiredState
+$out = dsc -l trace config test -i $yaml 2>"$testdrive/error.log" | Out-String
+$LASTEXITCODE | Should -Be 2
+$out | Should -BeNullOrEmpty
+(Get-Content -Path "$testdrive/error.log" -Raw) | Should -BeLike "*ERROR*Credential object 'Credential' requires both 'username' and 'password' properties*" -Because (Get-Content -Path "$testdrive/error.log" -Raw | Out-String)
 
 }
 
