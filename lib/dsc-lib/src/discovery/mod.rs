0712dbf190f111ad75ba7457074eda75886851c1
Steve Lee <slee@microsoft.com>
2026-01-27 09:57:11 -0800

    address Tess' comment to simplify the code

--- lib/dsc-lib/src/discovery/mod.rs
+++ lib/dsc-lib/src/discovery/mod.rs
@@ -139,13 +139,12 @@ impl Discovery {
         if !self.refresh_cache && !self.resources.is_empty() {
             // If resources are already discovered, no need to re-discover.
             return Ok(());
-        } else if self.refresh_cache {
-            self.resources.clear();
-            self.extensions.clear();
         }
 
         let mut command_discovery = CommandDiscovery::new(progress_format);
         if self.refresh_cache {
+            self.resources.clear();
+            self.extensions.clear();
             command_discovery.set_discovery_mode(&ResourceDiscoveryMode::DuringDeployment);
         }
         let discovery_types: Vec<Box<dyn ResourceDiscovery>> = vec![
