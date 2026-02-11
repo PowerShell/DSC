29db4fce4cc8acb8a2f6ba6ffa2c8a99ebe17074
Steve Lee <slee@microsoft.com>
2026-02-11 14:41:59 -0800

    Separate Microsoft.DSC metadata into `directives` and `executionInformation`

--- dsc/tests/dsc_version.tests.ps1
+++ dsc/tests/dsc_version.tests.ps1
@@ -17,4 +17,40 @@ Describe 'tests for metadata versioning' {
         $dscVersion = (dsc --version).Split(" ")[1]
         $version | Should -Be $dscVersion
     }
+
+    It 'returns error if configuration requires higher DSC version' {
+        $config_yaml = @"
+            `$schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
+            directives:
+              version: 999.0.0
+            resources:
+            - name: Echo
+              type: Microsoft.DSC.Debug/Echo
+              properties:
+                output: 'Hello, World!'
+"@
+        $null = $config_yaml | dsc config get -f - 2>$testdrive/error.log
+        $errorLog = Get-Content -Path $testdrive/error.log -Raw
+        $errorLog | Should -BeLike "*Validation*Configuration requires DSC version '999.0.0', but the current version is '*"
+        $LASTEXITCODE | Should -Be 2
+    }
+
+    It 'returns no error if DSC version satisfies configuration requirement' {
+        $config_yaml = @"
+            `$schema: https://aka.ms/dsc/schemas/v3/bundled/config/document.json
+            directives:
+              version: '>=3.1'
+            resources:
+            - name: Echo
+              type: Microsoft.DSC.Debug/Echo
+              properties:
+                output: 'Hello, World!'
+"@
+        $out = $config_yaml | dsc config get -f - 2>$testdrive/error.log
+        $errorLog = Get-Content -Path $testdrive/error.log -Raw
+        $errorLog | Should -BeNullOrEmpty
+        $LASTEXITCODE | Should -Be 0
+        $result = $out | ConvertFrom-Json
+        $result.results[0].result.actualState.output | Should -BeExactly 'Hello, World!' -Because $out
+    }
 }
