fb709ff2f1ccd48a67148eb152ac6d0a81175a0d
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2026-03-05 14:45:27 -0800

    Update from ms-prod-1.88 to ms-prod-1.93
    The version ms-stable stopped being supported in 2024 so we've been on an old toolchain.

--- helpers.build.psm1
+++ helpers.build.psm1
@@ -370,7 +370,7 @@ function Get-RustUp {
         if ($null -ne (Get-Command msrustup -CommandType Application -ErrorAction Ignore)) {
             Write-Verbose -Verbose "Using msrustup"
             $rustup = 'msrustup'
-            $channel = 'ms-stable'
+            $channel = 'ms-prod-1.93'
             if ($architecture -eq 'current') {
                 $env:MSRUSTUP_TOOLCHAIN = "$architecture"
             }
