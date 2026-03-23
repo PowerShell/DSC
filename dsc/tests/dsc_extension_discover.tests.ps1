9f962b970fcbdcefe528c8d0614fd6688f89c890
G.Reijn <gijsreijn@hotmail.com>
2026-03-23 04:58:51 +0100

    Swap order

--- dsc/tests/dsc_extension_discover.tests.ps1
+++ dsc/tests/dsc_extension_discover.tests.ps1
@@ -25,11 +25,11 @@ Describe 'Discover extension tests' {
         $LASTEXITCODE | Should -Be 0
         if ($IsWindows) {
             $out.Count | Should -Be 3 -Because ($out | Out-String)
-            $out[0].type | Should -Be 'Microsoft.Windows.Appx/Discover'
-            $out[0].version | Should -Be '0.1.0'
+            $out[0].type | Should -BeExactly 'Microsoft.PowerShell/Discover'
+            $out[0].version | Should -BeExactly '0.1.0'
             $out[0].capabilities | Should -BeExactly @('discover')
             $out[0].manifest | Should -Not -BeNullOrEmpty
-            $out[1].type | Should -BeExactly 'Microsoft.PowerShell/Discover'
+            $out[1].type | Should -BeExactly 'Microsoft.Windows.Appx/Discover'
             $out[1].version | Should -BeExactly '0.1.0'
             $out[1].capabilities | Should -BeExactly @('discover')
             $out[1].manifest | Should -Not -BeNullOrEmpty
