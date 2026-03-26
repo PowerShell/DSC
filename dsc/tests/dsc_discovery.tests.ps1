1ef96688445479bf165df895347ac14d48bb19ea
Mikey Lombardi <mlombardi@microsoft.com>
2026-03-26 13:49:54 -0500

    (GH-538) Fix acceptance tests
    This change addresses failing acceptance tests caused by the
    update to version parsing and handling.

--- dsc/tests/dsc_discovery.tests.ps1
+++ dsc/tests/dsc_discovery.tests.ps1
@@ -102,7 +102,7 @@ Describe 'tests for resource discovery' {
 '@
             Set-Content -Path "$testdrive/test.dsc.resource.json" -Value $manifest
             $null = dsc resource list 2> "$testdrive/error.txt"
-            "$testdrive/error.txt" | Should -FileContentMatchExactly 'WARN.*?does not use semver' -Because (Get-Content -Raw "$testdrive/error.txt")
+            "$testdrive/error.txt" | Should -FileContentMatchExactly 'WARN.*?invalid semantic version' -Because (Get-Content -Raw "$testdrive/error.txt")
         }
     }
 
