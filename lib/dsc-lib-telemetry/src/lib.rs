b760d708247996f439930d72bd0acac84b003075
Mikey Lombardi <mlombardi@microsoft.com>
2026-08-10 13:02:45 -0500

    (GH-1061) Encapsulate tracing initialization in telemetry crate
    Prior to this change, `dsc` initialized tracing directly. Other crates,
    like `registry`, follow similar steps to initialize tracing.
    
    This change extracts the tracing initialization logic into the telemetry
    crate in the `basic` module. This module defines a `BasicTracingOptions`
    struct that can create a default guard and initialize a subscriber.
    
    This change also re-exports the tracing crates from within the
    `basic` module to enable taking a single dependency.

--- lib/dsc-lib-telemetry/src/lib.rs
+++ lib/dsc-lib-telemetry/src/lib.rs
@@ -2,11 +2,24 @@
 // Licensed under the MIT License.
 
 //! This library provides telemetry support for DSC.
-//! 
-//! It includes functionality for OpenTelemetry tracing, metrics, and exporting telemetry data to various backends.
-//! Other crates can take a dependency on this crate to leverage its OTel capabilities and keep a consistent
-//! implementation and dependencies.
-//! 
-//! This crate also supports basic tracing functionality for applications that do not require full OpenTelemetry
-//! support. This functionality bundles the [`tracing`] crate and related crates to provide a simple way to emit trace
-//! events to stderr in a human-readable format.
+//!
+//! It includes functionality for OpenTelemetry tracing, metrics, and exporting telemetry data to
+//! various backends. Other crates can take a dependency on this crate to leverage its OTel
+//! capabilities and keep a consistent implementation and dependencies.
+//!
+//! This crate also supports basic tracing functionality for applications that do not require full
+//! OpenTelemetry support. This functionality bundles the [`tracing`] crate and related crates to
+//! provide a simple way to emit trace events to stderr in a human-readable format.
+
+pub mod basic;
+
+/// Represents the different formats that can be used for stderr output in tracing.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum StderrFormat {
+    /// Emit trace events as human-readable text to stderr with ANSI color codes.
+    Default,
+    /// Emit trace events as human-readable text to stderr without ANSI color codes.
+    Plaintext,
+    /// Emit trace events as JSON to stderr.
+    Json,
+}
