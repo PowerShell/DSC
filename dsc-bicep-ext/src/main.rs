d5a24a5cee3b69e4a2a72a9ece3eeec3c78c9c49
Steve Lee (POWERSHELL HE/HIM) (from Dev Box) <slee@ntdev.microsoft.com>
2026-09-14 14:04:45 -0700

    Fix bicep extension to require transport to be specified otherwise return error

--- dsc-bicep-ext/src/main.rs
+++ dsc-bicep-ext/src/main.rs
@@ -460,8 +460,9 @@ async fn run_server(
         return Ok(());
     }
 
-    // Default to HTTP server on [::1]:50051 if no transport specified
-    let addr = http.unwrap_or_else(|| "[::1]:50051".to_string());
+    let Some(addr) = http else {
+        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, t!("bicep.transportNotSpecified"))));
+    };
     tracing::info!(
         "{}",
         t!(
